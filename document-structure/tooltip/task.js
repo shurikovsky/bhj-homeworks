5337cb8da9e6167b958ebc591cc1782bbd0452cb
Kryukov Alexandr <shurikovsky@mail.ru>
2025-01-30 17:35:11 +0500

    it work

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,28 +1,25 @@
 const hasTooltip = [... document.getElementsByClassName('has-tooltip')];
-const title = [];
-
+let div = document.createElement('div');
 hasTooltip.forEach((item, index, array) => {
     item.addEventListener('click', (e) => {
       e.preventDefault();
-      console.log(item, item.nextElementSibling);
+      if (item.getAttribute('title') === div.textContent) {
+        div.classList.toggle('tooltip_active');
+      } else {
+      div.classList.remove('tooltip_active');
       if ((!item.nextElementSibling) || (item.nextElementSibling.className !== 'tooltip')) {
-        let div = document.createElement('div');
+        div = document.createElement('div');
         item.insertAdjacentElement('afterend', div);
-        div.outerHTML = '<div class="tooltip" style="left: 0; top: 0">' + item.getAttribute('title') + '</div>';
-        title[index] = div;
-      } 
-        //if (item.getAttribute('title') === title[index].textContent) {
-        //  title[index].classList.toggle('tooltip_active');
-        //} else { 
-          title.forEach((item) => {
-            item.classList.remove('tooltip_active');
-          })
-          let rect = item.getBoundingClientRect();
-          console.log(rect);//проверка какие координаты пишет
-          title[index].classList.add('tooltip_active');
-          title[index].style.top = rect.top + 20 + 'px';
-          title[index].style.left = rect.left + 'px';
-         
+        div.outerHTML = '<div class="tooltip" style="left: 0; top: 0"></div>';      
+        div = item.nextElementSibling;
+      }
+        let rect = item.getBoundingClientRect();
+        div.style.top = rect.top + 20 + 'px';
+        div.style.left = rect.left + 'px';
+        div.textContent = item.getAttribute('title');
+        div.classList.add('tooltip_active');
+        }
+          
     });
 })
 
