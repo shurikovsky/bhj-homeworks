18aab5dc68ba6b765a91317e1b7d613b63530dbf
Kryukov Alexandr <shurikovsky@mail.ru>
2025-02-10 16:10:54 +0300

    progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,12 +1,14 @@
 const form = document.forms.form;
 const labelProgress = document.getElementById("progress");
 form.addEventListener('submit', (e) => {
-  e.preventDevault();
+  e.preventDefault();
   let xhr = new XMLHttpRequest();
   xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
   let data = new FormData(form);
-  xhr.send(data);
   xhr.upload.addEventListener('progress', (e) => {
-    labelProgress.value = e.progress;
+    console.log(e.loaded);
+    console.log(e.total);
+    labelProgress.value = e.loaded / e.total
   })
+  xhr.send(data);
 })
